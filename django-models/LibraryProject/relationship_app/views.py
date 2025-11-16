e204f67a123a155149ce9b59207cafdef99bbd33
skaduteye <skaduteye@gmail.com>
2025-11-16 23:23:42 +0000

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,32 +1,6 @@
-from django.shortcuts import render
-from django.views.generic.detail import DetailView
-from .models import Library, Book
-
-
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
-
-
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
-
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
-
-
-def user_login(request):
-    from django.contrib.auth.views import LoginView
-    return LoginView.as_view(template_name='relationship_app/login.html')(request)
-
-
-def user_logout(request):
-    from django.contrib.auth.views import LogoutView
-    return LogoutView.as_view(template_name='relationship_app/logout.html')(request)
-
 
 def register(request):
     form = UserCreationForm()
