ccfd48010f8efe9e55b8c166ac4d1d1083c7899a
skaduteye <skaduteye@gmail.com>
2025-11-16 23:48:31 +0000

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -29,19 +29,20 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render
 
-# Admin view
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
+
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
+
 @user_passes_test(lambda u: hasattr(u, 'userprofile') and u.userprofile.role == 'Admin')
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
 
-
-# Librarian view
 @user_passes_test(lambda u: hasattr(u, 'userprofile') and u.userprofile.role == 'Librarian')
 def librarian_view(request):
     return render(request, 'relationship_app/librarian_view.html')
 
-
-# Member view
 @user_passes_test(lambda u: hasattr(u, 'userprofile') and u.userprofile.role == 'Member')
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
