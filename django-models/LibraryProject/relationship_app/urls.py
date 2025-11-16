47cb2667ae4d44712c2e2f686e9505519e658ce3
skaduteye <skaduteye@gmail.com>
2025-11-16 23:41:44 +0000

    new files

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -13,3 +13,8 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
 ]
+
+# Role-based URLs
+path('admin-view/', views.admin_view, name='admin_view'),
+path('librarian-view/', views.librarian_view, name='librarian_view'),
+path('member-view/', views.member_view, name='member_view'),
