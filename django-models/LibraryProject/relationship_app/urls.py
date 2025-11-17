7356b06e86ad2c29348390d2321948a0c0911de5
skaduteye <skaduteye@gmail.com>
2025-11-17 00:04:45 +0000

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -40,8 +40,8 @@ urlpatterns = [
     path('member-view/', member_view, name='member_view'),
 
     # Book permissions URLs
-    path('books/add/', views.add_book, name='add_book'),
-    path('books/<int:pk>/edit/', views.edit_book, name='edit_book'),
-    path('books/<int:pk>/delete/', views.delete_book, name='delete_book'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/', views.delete_book, name='delete_book'),
 ]
 
