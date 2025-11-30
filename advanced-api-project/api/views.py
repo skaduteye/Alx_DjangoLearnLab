46f80fbef1358cc3f298a3a504b83c82e058e08c
skaduteye <skaduteye@gmail.com>
2025-11-30 23:11:32 +0000

    Fix import statement for django_filters to match expected format

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics, permissions
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
 from rest_framework.filters import SearchFilter, OrderingFilter
 from .models import Book
 from .serializers import BookSerializer
@@ -34,7 +34,7 @@ class BookListView(generics.ListAPIView):
     permission_classes = [permissions.AllowAny]  # Public read access
     
     # Enable filtering, searching, and ordering
-    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filter_backends = [rest_framework.DjangoFilterBackend, SearchFilter, OrderingFilter]
     
     # Specify fields that can be filtered
     filterset_fields = ['title', 'author__name', 'publication_year']
